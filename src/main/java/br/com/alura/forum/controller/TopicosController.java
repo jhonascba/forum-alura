e336d766596c4feaf6f7726a158456d87036557e
jhonascba <jhonascbarbosa@gmail.com>
2022-09-05 18:08:06 -0300

    trabalhando com post

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,29 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso) {
         if (nomeCurso == null) {
             List<Topico> topicos = topicoRepository.findAll();
@@ -25,5 +31,14 @@ public class TopicosController {
         }
         List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
         return TopicoDto.converter(topicos);
+    }
+
+    @PostMapping
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+        Topico topico = form.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
     }
 }
