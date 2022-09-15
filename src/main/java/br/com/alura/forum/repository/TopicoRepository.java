ce9d4cc4bc67197b439f63b6186ca1990ce37db8
jhonascba <jhonascbarbosa@gmail.com>
2022-09-15 20:32:42 -0300

    adicionando listagem ao projeto

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,6 +1,8 @@
 package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +16,7 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
     //Busca por relacionamento - Curso se refere a entidade, e nome ao atributo da entidade
     //também é possível utilizar o padrão Entidade_Atributo
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
     //Se não não respeitar o padrão acima, deverá implementar a query por conta própria
     @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
