981891c9c2ca9e1650bb7487731b2c5771007ac5
jhonascba <jhonascbarbosa@gmail.com>
2022-09-19 19:24:13 -0300

    Restringindo o acesso aos endpoints privados

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -16,9 +16,17 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                        .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .authorizeHttpRequests((authz) -> {
+                            try {
+                                authz
+                                        .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                                        .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                                        .anyRequest().authenticated()
+                                        .and().formLogin();
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
                 )
                 .httpBasic(withDefaults());
         return http.build();
